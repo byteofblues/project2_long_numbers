4307434abe61ff48d9f02f617076e947908b2416
James Pai <jamespai77@gmail.com>
2023-02-21 10:05:33 -0500

    Refactor Number class to improve functionality and readability. Add custom exceptions for null input and non-digit characters, and implement a linked list using Node class for number representation.

--- Number.java
+++ Number.java
@@ -1,5 +1,6 @@
 public class Number {
-    private int number;
+    private Node head;
+    private Node tail;
     private int digitsCount;
 
 
@@ -7,10 +8,8 @@ public class Number {
 
 
     public Number(String number) throws NullPointerException, IllegalArgumentException {
-        isNull(number);
-        isValidNumber(number);
-        setIntValue(number);
-        this.digitsCount = number.length();
+        createLinkedListFromNumber(number);
+        this.digitsCount = 0;
     }
     
 
@@ -20,28 +19,110 @@ public class Number {
     }
 
 
-    private void isValidNumber(String number) throws IllegalArgumentException {
+    private void isValidDigit(String number) throws IllegalArgumentException {
         String digits = "[0-9]+";
-            if ( !(number.matches(digits)) ) {
-                throw new IllegalArgumentException("invalid number: the input string contains non-digit characters");
-            }           
+        if ( !(number.matches(digits)) ) 
+            throw new IllegalArgumentException("invalid number: the input string contains non-digit characters");          
     }
 
 
-    private void setIntValue(String number) {
+    private int setIntValue(String number) {
         int parsedNum = Integer.parseInt(number);
-        this.number = parsedNum;
+        return parsedNum;
+    }
+    
+
+    private void createLinkedListFromNumber(String number) throws NullPointerException, IllegalArgumentException {
+        // check if number is not null
+        isNull(number);
+
+        this.head = null;
+        this.tail = null;
+        // Node current = head;
+
+        int l = number.length();
+        for (int i = l; i > 0; i--) {
+            String digit = number.charAt(i - 1) + "";
+            // check if character is a decimal digit
+            isValidDigit(digit);
+
+            // create a new node starting from the least significant digit, or the ones digit
+            // method for linked list implementation
+            Node n = new Node(setIntValue(digit));
+
+            // least significant digit stored as head
+            // stores the first node as head
+            if (this.head == null) {
+                this.head = n;
+                this.tail = n; 
+            }
+
+            // append the next least significant digit to the list
+            else {
+                this.tail.next = n;
+                this.tail = n;
+            }
+
+            this.digitsCount++;
+        }
     }
 
-
+    /*
+     * compares two Number objects, returns equal
+     * if all the nodes in the linked list contain
+     * the same integer value, in the same order
+     */
+    // @Override
     // public int compareTo​(Number other) throws NullPointerException {
     //     return 0;
     // }
 
 
+    public String printNumber() {
+        String s = "";
+        Node current = this.head;
+
+        while (current != null) {
+            s += current.data;
+            current = current.next;
+        }
+
+        s = reverseString(s);
+
+        return s;
+    }
+
+
+    public String reverseString(String str) {
+        String reversedStr = "";
+        for (int i = str.length(); i > 0; i--) {
+            reversedStr += str.charAt(i - 1);
+        }
+
+        return reversedStr;
+    }
+
+
     @Override 
     public String toString() {
-        return Integer.toString(this.number);
+        // return Integer.toString(this.number);
+        return printNumber();
     }
+
+
+    /*
+     * define the internal Node class, constructs
+     * a Node that stores a single digit from its 
+     * series of digits
+     */
+    private class Node {
+        public int data;
+        public Node next;
+
+        public Node (int number) {
+            this.data = number;
+            this.next = null;
+        }
+    }   
 
 }
